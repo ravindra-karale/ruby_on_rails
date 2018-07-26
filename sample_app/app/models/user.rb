510ea7ef8506d7f673487f496a2607ad37553c75
ravindra-karale <ravindra.karale@wgblindia.com>
2018-07-26 16:56:49 +0530

    chapter-13

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -54,7 +54,6 @@ class User < ApplicationRecord
 
    # Sets the password reset attributes.
   def create_reset_digest
-    # self.reset_token = User.new_token
     # update_columns(reset_digest:  FILL_IN, reset_sent_at: FILL_IN)
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
